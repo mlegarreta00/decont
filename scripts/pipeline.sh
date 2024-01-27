b869134d37ab1fcbfe4f26fd9c063eb3380cb615
María Legarreta <maria.legarretauriarte@gmail.com>
2024-01-27 10:56:36 +0100

    Modificar scripts

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,67 +1,58 @@
 #!/bin/bash
 
-#Download all the files specified in data/filenames
+# Download all the files specified in data/urls
 for url in $(<data/urls) #TODO
 do
-    bash scripts/download.sh $url data
+    bash scripts/download.sh "$url" data
 done
 
 # Download the contaminants fasta file, uncompress it, and
 # filter to remove all small nuclear RNAs
-bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes #TODO
+contaminants_url="https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz"
+bash scripts/download.sh "$contaminants_url" res yes #TODO
 
 # Index the contaminants file
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
 # Merge the samples into a single file
-for sid in $(ls data/*fastq.gz | sed 's/.\///' | cut -d '.' -f1 | sort -u); #TODO
-do
-    bash scripts/merge_fastqs.sh data out/merged $sid
+for filepath in data/*fastq.gz; do
+    # Obtain the filename without path and extension
+    filename=$(basename "$filepath")
+    # Use sed to transform the filename
+    sample_id=$(echo "$filename" | sed -E 's/-12\.5dpp\.1\.[12]s_sRNA.*//;s/\..*//')
+    # Execute the merge script
+    bash scripts/merge_fastqs.sh data out/merged "$sample_id"
 done
 
-# TODO: run cutadapt for all merged files
+# Execute cutadapt for all merged files
+log_file="log/pipeline.log"
+mkdir -p log/cutadapt  # Create log/cutadapt directory if it doesn't exist
+mkdir -p out/trimmed
+for merged_file in out/merged/*.fastq.gz; do
+    trimmed_file="out/trimmed/$(basename "$merged_file" .fastq.gz).trimmed.fastq.gz"
+    cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
+        -o "$trimmed_file" "$merged_file" > log/cutadapt/$(basename "$merged_file" .fastq.gz).log
 
-
-for infile in out/merged/*.fastq.gz
-do
-sid=$(basename "$infile".fastq.gz)
-trimmed_file="out/trimmed/${sid}.trimmed.fastq.gz"
-log_file="log/cutadapt/${sid}.log"
-cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
-     -o "$trimmed_file" "$infile" > "$log_file"
+    # Add relevant information to the log file
+    echo "Cutadapt results for $(basename "$merged_file"):" >> "$log_file"
+    grep -E 'Reads with adapters|Total basepairs' log/cutadapt/$(basename "$merged_file" .fastq.gz).log >> "$log_file"
 done
 
-# TODO: run STAR for all trimmed files
-for fname in out/trimmed/*.fastq.gz
-do
-    # you will need to obtain the sample ID from the filename
-    sid=$(basename "$fname".trimmed.fastq.gz)
+# Execute STAR for all trimmed files
+mkdir -p out/star
+for trimmed_file in out/trimmed/*.fastq.gz; do
+    sid=$(basename "$trimmed_file" .trimmed.fastq.gz)
     output_directory="out/star/$sid"
     mkdir -p "$output_directory"
-    STAR --runThreadN 4 --genomeDir res/filtered_contaminants_idx \
-        --outReadsUnmapped Fastx --readFilesIn "$trimmed_file" \
-       --readFilesCommand gunzip -c --outFileNamePrefix "$output_directory"
-done 
+    STAR --runThreadN 4 --genomeDir res/contaminants_idx \
+         --outReadsUnmapped Fastx --readFilesIn "$trimmed_file" \
+         --readFilesCommand gunzip -c --outFileNamePrefix "$output_directory/" > temp_star.log
 
-# TODO: create a log file containing information from cutadapt and star logs
-# (this should be a single log file, and information should be *appended* to it on each run)
-# - cutadapt: Reads with adapters and total basepairs
-# - star: Percentages of uniquely mapped reads, reads mapped to multiple loci, and to too many loci
-# tip: use grep to filter the lines you're interested in
-for sid in $(basename -s .trimmed.fastq.gz -a out/trimmed/*.fastq.gz | sort -u) # Extract unique sample IDs
-do
-    cutadapt_log="log/cutadapt/${sid}.log"
-    star_log="out/star/${sid}/Log.final.out"
-    pipeline_log="log/pipeline.log"
-
-cutadapt_info=$(grep "Reads with adapters" "$cutadapt_log")
-cutadapt_bp_info=$(grep "Total basepairs processed" "$cutadapt_log")
-
-star_info=$(grep -E "Uniquely mapped reads|Number of reads mapped to multiple loci|Number of reads mapped to too many loci" "$star_log")
-
- echo "Sample: $sid" >> "$pipeline_log"
-    echo "Cutadapt Info: $cutadapt_info" >> "$pipeline_log"
-    echo "Cutadapt Basepair Info: $cutadapt_bp_info" >> "$pipeline_log"
-    echo "STAR Info: $star_info" >> "$pipeline_log"
-    echo "----------------------------------------" >> "$pipeline_log"
+    # Add relevant information to the log file
+    echo "STAR results for $sid:" >> "$log_file"
+    grep -E 'Uniquely mapped reads %|Number of reads mapped to multiple loci|% of reads mapped to too many loci' temp_star.log >> "$log_file"
 done
+
+# Clean up temporary log files
+rm temp_star.log
+
